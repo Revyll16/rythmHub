d0efd2ba7306c4c7c3e00f05989bcc6b536090cf
michael uche <bonjourfamily28@gmail.com>
2024-12-03 14:20:05 +0200

    the musiciansindex page

--- app/controllers/musicians_controller.rb
+++ app/controllers/musicians_controller.rb
@@ -1,2 +1,33 @@
 class MusiciansController < ApplicationController
-end
+
+    def index
+      @musicians = Musician.all
+    end
+
+    
+    def show
+      @musician = Musician.find(params[:id])
+    end
+
+    
+    def new
+      @musician = Musician.new
+    end
+
+    
+    def create
+      @musician = Musician.new(musician_params)
+      if @musician.save
+        redirect_to @musician, notice: 'Musician was successfully created.'
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    
+    def musician_params
+      params.require(:musician).permit(:name, :genre, :instrument, :bio)
+    end
+  end
