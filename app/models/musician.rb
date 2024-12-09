5041793a3f56f602f13491e81325831740bdb8df
LauraSW-dev <systemsengineer707@gmail.com>
2024-12-09 17:33:32 +0200

    Update musician.rb

--- app/models/musician.rb
+++ app/models/musician.rb
@@ -1,7 +1,7 @@
 class Musician < ApplicationRecord
   belongs_to :user
   has_many :compositions, dependent: :destroy
-  has_many :instruments
+  has_and_belongs_to_many :instruments
   has_many :feedbacks
 
   validates :image_url, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: "must be a valid URL" }, allow_blank: true
